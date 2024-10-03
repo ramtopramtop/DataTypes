76201a399f185d51dd46fc4ec7f37402d36578af
ramtopramtop <ramtopramtop@rambler.ru>
2024-10-03 18:08:33 +0300

    4 task too long

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  let count = {};
+  for (element of arr) {
+    if (typeof (element) === 'number') {
+      count.number += 1;
+    }
+    else {
+      if (typeof (element) === 'string') {
+        count.string += 1;
+      }
+      else {
+        count.boolean += 1;
+      }
+    }
+  }
+  return count;
+};
 
 module.exports = { countTypesInArray };
